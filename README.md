188a15b512d2a5baa7277cf66b741ed4599236be
sylw <sylwiaglowienka14@gmail.com>
2023-05-30 12:20:01 +0200

    About redirection

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-<!-- News homepage  -->
-
-This challenge was made thanks to Forntend Mentor website!
-
+GARWOLANKA swimming pool
+The new website of the Garwolanka swimming pool is fully written and designed by me, including the graphics attached to the website. It is a simple site with new functionalities, using e.g. CSS grid. The site is fully responsive and follows the Green Digital principles.
--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const mobileMenu = document.querySelector(".button");
-const menuList = document.querySelector(".menu-list");
 const darkMode = document.querySelector(".dark-layer");
 const menuElements = document.querySelector(".menu-list");
-const closeIcon = document.querySelector(".menu-list img");
 const popup = document.querySelector(".popup");
 const popupPrices = document.querySelector(".popupPrices");
 const contact = document.getElementById("contact");
 const prices = document.querySelectorAll(".prices");
+const schoolPopup = document.querySelector(".schoolPopup");
+const school = document.querySelectorAll(".school");
+const reserv = document.querySelectorAll(".reserv");
+const reservPopup = document.querySelector(".popupReserv");
+const redButton = document.querySelector(".about");
 
+function redirectToAboutPage() {
+  window.location.href = "about.html";
+}
+function redirectToMain() {
+  window.location.href = "index.html";
+}
 contact.addEventListener("click", () => {
   popup.style.display = "block";
   darkMode.classList.toggle("active");
@@ -30,20 +39,70 @@ prices.forEach((price) => {
 });
 
 function closePrices(event) {
-  const pricesArray = Array.from(prices)
-  if (!popupPrices.contains(event.target) && !pricesArray.includes(event.target)) {
+  const pricesArray = Array.from(prices);
+  if (
+    !popupPrices.contains(event.target) &&
+    !pricesArray.includes(event.target)
+  ) {
     popupPrices.style.display = "none";
     darkMode.classList.toggle("active");
     window.removeEventListener("click", closePrices);
   }
 }
+
+school.forEach((schl) => {
+  schl.addEventListener("click", () => {
+    schoolPopup.style.display = "block";
+    darkMode.classList.toggle("active");
+    window.addEventListener("click", closeSchool);
+  });
+});
+
+function closeSchool(event) {
+  const schoolArray = Array.from(school);
+  if (
+    !schoolPopup.contains(event.target) &&
+    !schoolArray.includes(event.target)
+  ) {
+    schoolPopup.style.display = "none";
+    darkMode.classList.toggle("active");
+    window.removeEventListener("click", closeSchool);
+  }
+}
+
+reserv.forEach((rv) => {
+  rv.addEventListener("click", () => {
+    reservPopup.style.display = "block";
+    darkMode.classList.toggle("active");
+    window.addEventListener("click", closeReserv);
+  });
+});
+
+function closeReserv(event) {
+  const reservArray = Array.from(reserv);
+  if (
+    !reservPopup.contains(event.target) &&
+    !reservArray.includes(event.target)
+  ) {
+    reservPopup.style.display = "none";
+    darkMode.classList.toggle("active");
+    window.removeEventListener("click", closeReserv);
+  }
+}
+
 mobileMenu.addEventListener("click", () => {
   darkMode.classList.toggle("active");
   menuElements.style.display = "block";
-  closeIcon.addEventListener("click", closeMenu);
+  window.addEventListener("click", closeMenu);
 });
 
-const closeMenu = () => {
-  darkMode.classList.toggle("active");
-  menuElements.style.display = "none";
-};
+function closeMenu(event) {
+  if (
+    !menuElements.contains(event.target) &&
+    !mobileMenu.includes(event.target)
+  ) {
+    darkMode.classList.toggle("active");
+    menuElements.style.display = "none";
+    window.removeEventListener("click", closeMenu);
+  }
+}
